c6835337fe5e1b15126647a24ec91b22cb3e6daa
Rene Cocom <fasnimo@gmail.com>
2019-09-10 04:30:41 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,5 +24,5 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, character_books.character_id FROM character_books
-  JOIN characters ON characters.id = character_books.character_id GROUP BY character_books.character_id ORDER BY characters.name ACS"
+  JOIN characters ON characters.id = character_books.character_id GROUP BY character_books.character_id ORDER BY characters.name ASC"
 end
