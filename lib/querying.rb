6835f6b8ba1dd70410cb179973a43f73d7df997d
Rene Cocom <fasnimo@gmail.com>
2019-09-10 03:44:54 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN books ON books.series_id = character_books.book_id
+  "SELECT series.title FROM series JOIN books ON books.series_id = series.id
    WHERE characters.species = 'human' LIMIT 1"
 end
 
