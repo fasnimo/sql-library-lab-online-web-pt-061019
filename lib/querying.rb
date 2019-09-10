dbafaa730bdda0c976cf48b1844dc9a42efa5fbe
Rene Cocom <fasnimo@gmail.com>
2019-09-10 03:47:34 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,8 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series JOIN books ON books.series_id = series.id
-   WHERE characters.species = 'human' LIMIT 1"
+   JOIN character_books ON character_books.book_id = books.id 
+   "#WHERE characters.species = 'human' LIMIT 1
 end
 
 def select_character_names_and_number_of_books_they_are_in
