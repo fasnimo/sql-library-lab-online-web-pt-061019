00ee0bcbec81b127f16779eafef81f899bf7ad1f
Rene Cocom <fasnimo@gmail.com>
2019-09-10 04:11:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,5 +23,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name FROM character_books ON character_books.character_id = characters.id"
+  "SELECT characters.name, SUM(character_books.character_id) FROM character_books ON character_books.character_id = characters.id"
 end
