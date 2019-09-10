8f770a024a3d07346c25239f62ddbb4ea25303be
Rene Cocom <fasnimo@gmail.com>
2019-09-10 04:45:48 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,6 +23,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, character_books.book_id FROM characters
-  JOIN character_books ON characters.id = character_books.character_id JOIN books ON character_books.book_id = books.id GROUP BY characters.name ORDER BY character_books.book_id DESC"
+  "SELECT characters.name, COUNT(*) AS character_in FROM character_books
+  JOIN characters ON characters.id = character_books.character_id JOIN books ON character_books.book_id = books.id GROUP BY characters.name ORDER BY character_books.book_id DESC"
 end
