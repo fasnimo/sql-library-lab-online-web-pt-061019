0d7635625a3c08333437fb7d83feabd3d51fe632
Rene Cocom <fasnimo@gmail.com>
2019-09-10 03:08:56 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(*) FROM characters COUNT(species) GROUP BY characters.species DESC"
+  "SELECT characters.species, COUNT(*) FROM characters GROUP BY species ORDER BY COUNT(species) DESC "
 end
 
 def select_name_and_series_subgenres_of_authors
